d84f13c2517c57f21e1c25db970b6f580dbd029c
krishan1298 <krishanbha129@gmail.com>
2023-11-27 11:40:42 -0500

    added saveGame() and loadGame() (Commented out) code. Not final.

--- src/main/java/org/example/SaveData.java
+++ src/main/java/org/example/SaveData.java
@@ -2,21 +2,34 @@ package org.example;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
 public class SaveData {
     private Player player;
+    private ArrayList<Room> rooms;
+    private ArrayList<Monster> monsters;
+    private ArrayList<Puzzle> puzzles;
 
     private transient Map map;
 
     // default constructor
     public SaveData() {
+
     }
 
     // constructors, getters, and setters
 
-    public SaveData(Player player, Map map) {
+    public SaveData(Player player, ArrayList<Room> rooms, ArrayList<Monster> monsters, ArrayList<Puzzle> puzzles, Map map) {
         this.player = player;
+        this.rooms = rooms;
+        this.monsters = monsters;
+        this.puzzles = puzzles;
         this.map = map;
     }
+
 
     // getters and setters
 
@@ -36,5 +49,24 @@ public class SaveData {
         this.map = map;
     }
 
-    // ... other methods ...
+    public void saveGame(){
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("gamestate.dat"))) {
+            oos.writeObject(this);
+            System.out.println("Game state saved successfully!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //loading game I got from chatgpt
+//    public static Game loadGame() {
+//        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("gamestate.dat"))) {
+//            Game loadedGame = (Game) ois.readObject();
+//            System.out.println("Game state loaded successfully!");
+//            return loadedGame;
+//        } catch (IOException | ClassNotFoundException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
+//}
 }
